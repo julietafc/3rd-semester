d03d86f132d6c161aa1561739d7f281aa2e7150a
julietafc <juli47g1@stud.kea.dk>
2021-08-30 21:21:19 +0200

    capitalization3

--- sandbox/capitalization3.js
+++ sandbox/capitalization3.js
@@ -3,6 +3,3 @@
 name = "Peter";
 
 console.log(name.substring(0, 2).toLowerCase() + name.substring(2, 3).toUpperCase() + name.substring(3).toLowerCase());
-
-// console.log (name[3].toUpperCase() + name.substring(1));
-// + name.substring(2).toUpperCase()
