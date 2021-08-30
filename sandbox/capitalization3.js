139642b7b8904229b5cbe056b4bbdaa6638a6561
julietafc <juli47g1@stud.kea.dk>
2021-08-30 19:37:34 +0200

    capitalization achieved

---
+++ sandbox/capitalization3.js
@@ -0,0 +1,8 @@
+"use strict";
+
+name = "Peter";
+
+console.log(name.substring(0, 2).toLowerCase() + name.substring(2, 3).toUpperCase() + name.substring(3).toLowerCase());
+
+// console.log (name[3].toUpperCase() + name.substring(1));
+// + name.substring(2).toUpperCase()
